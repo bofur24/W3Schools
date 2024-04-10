c28f0fcd6a02ca7c0e07d4da43fe9542ca80fa4a
Keith Heffington <lightingcount_@hotmail.com>
2024-04-10 13:35:44 -0500

    Completed Introduction for C#

---
+++ C-Sharp/Introduction-Home/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Introduction_Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn C#");
+            Console.WriteLine("C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework.");
+            Console.WriteLine("C# is used to develop web apps, desktop apps, mobile apps, games and much more.");
+            Console.WriteLine("----------");
+            Console.WriteLine("First Program");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("----------");
+            Console.WriteLine();
+            Console.WriteLine("Introduction");
+            Console.WriteLine("What is C#?");
+            Console.WriteLine("C# is pronounced \"C-Sharp\"");
+            Console.WriteLine("It is an object oriented programming language created by Microsoft that runs on the .NET Framework.");
+            Console.WriteLine("C# has roots from the C family, and the language is close to other popular languages like C++ aqnd Java.");
+            Console.WriteLine("The first version was released in year 2002. The latest version, C# 12, was released in November 2023");
+            Console.WriteLine("C# is used for: \nMobile applications \nDesktop applications \nWeb applications \nWeb services \nWeb sites \nGames \nVR \nDatabase applications \nAnd much, much more!");
+            Console.WriteLine("----------");
+            Console.WriteLine("Why use C#");
+            Console.WriteLine("It is one of the most popular programming languages in the world.");
+            Console.WriteLine("It is easy to learn and simple to use");
+            Console.WriteLine("It has huge community support");
+            Console.WriteLine("C# is an object-oriented language which gives a clear structure to programs and allows code to be reused, lowering development costs");
+            Console.WriteLine("As C# is close to C, C++ and Java, it makes it easy for programmers to switch to C# or vice versa");
+        }
+    }
+}
