0253b3968a763aa5a99a6b6af8b56f1cc403ad93
Keith Heffington <lightingcount_@hotmail.com>
2024-04-19 10:45:04 -0500

    Completed Conditionals-Switch

--- C-Sharp/Conditionals-Switch/Program.cs
+++ C-Sharp/Conditionals-Switch/Program.cs
@@ -89,8 +89,75 @@ namespace Conditionals_Switch
                 "the else condition sice condition1 and condition2 is both False - and print to the screen " +
                 "\"Good evening.\"");
             Console.WriteLine("However, if the time was 14, our program would print \"Good day.\"");
-
-
+            Console.WriteLine("");
+            Console.WriteLine("---------");
+            Console.WriteLine("C# Short hand If...Else (Ternary Operator)");
+            Console.WriteLine("There is also a short-hand if else, which is known as the \bternary operator\b becasue it consists of three operands." +
+                "It can be used to replace multiple lines of code with a single line. It is often used to replace simple if else statements:");
+            Console.WriteLine("You can simply write:");
+            Console.WriteLine();
+            Console.WriteLine("string result = (time < 18) ? \"Good day.\" : \"Good evening.\";");
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine("C# Switch Statements");
+            Console.WriteLine("Use the switch statement to select one of many code locks to be executed.");
+            Console.WriteLine("This is how it works: \n" +
+                "The switch expression is evaluated once\n" +
+                "The value of the expression is compared with the values of each case\n" +
+                "If there is a match, the associated block of code is executed\n" +
+                "The break and default keywords will be described later in this chapter");
+            Console.WriteLine("The example below uses the weekday number to calculate the weekday name:");
+            Console.WriteLine();
+            Console.WriteLine("            int day = 4;\r\n            switch (day) \r\n            {\r\n                case 1: \r\n                    Console.WriteLine(\"Monday\");\r\n                    break;\r\n                case 2:\r\n                    Console.WriteLine(\"Tuesday\");\r\n                    break;\r\n                case 3:\r\n                    Console.WriteLine(\"Wednesday\");\r\n                    break;\r\n                case 4:\r\n                    Console.WriteLine(\"Thursday\");\r\n                    break;\r\n                case 5:\r\n                    Console.WriteLine(\"Friday\");\r\n                    break;\r\n                case 6:\r\n                    Console.WriteLine(\"Saturday\");\r\n                    break;\r\n                case 7:\r\n                    Console.WriteLine(\"Sunday\");\r\n                    break;\r\n            }\r\n            // Outputs \"Thursday\" (day 4)\r\n\r\n\r\n        }");
+            int day = 4;
+            switch (day) 
+            {
+                case 1: 
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+            // Outputs "Thursday" (day 4)
+            Console.WriteLine();
+            Console.WriteLine("The break keyword");
+            Console.WriteLine("When C# reaches a break keyword, it breaks out of the switch block.");
+            Console.WriteLine("This will stop the execution of more coade and case testing inside the block.");
+            Console.WriteLine("When a match is found, and the job is done, it's time for a break. There is no need for more testing.");
+            Console.WriteLine();
+            Console.WriteLine("switch (day)\r\n            {\r\n                case 6:\r\n                    Console.WriteLine(\"Today is Saturday.\");\r\n                    break;\r\n                case 7:\r\n                    Console.WriteLine(\"Today is Sunday\");\r\n                    break;\r\n                default:\r\n                    Console.WriteLine(\"Looking forward to the weekend.\");\r\n                    break;\r\n            }\r\n            // Outputs \"Looking forward to the weekend.");
+            switch (day)
+            {
+                case 6:
+                    Console.WriteLine("Today is Saturday.");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the weekend.");
+                    break;
+            }
+            // Outputs "Looking forward to the weekend.
+            
 
         }
     }
